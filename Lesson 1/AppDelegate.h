fe8626b54d765643385ef7da8d72683c1142b3cc
Mefilt <mefilt@gmail.com>
2015-10-29 21:03:05 +0300

    lesson 1

--- Lesson 1/AppDelegate.h
+++ Lesson 1/AppDelegate.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import <FSCalendar/FSCalendar.h>
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
--- Lesson 1/AppDelegate.m
+++ Lesson 1/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"%@ ",    NSLocalizedString(@"test.home", @""));
     return YES;
 }
 
