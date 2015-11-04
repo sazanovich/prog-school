b0167a8f1dc3bf7deece80d1de88763156132055
alex <sazanovich.aleks@gmail.com>
2015-11-04 01:19:24 -0800

    Initial Commit

---
+++ LessonOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LessonOne
+//
+//  Created by alex on 04.11.15.
+//  Copyright © 2015 Programming School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LessonOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LessonOne
+//
+//  Created by alex on 04.11.15.
+//  Copyright © 2015 Programming School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
